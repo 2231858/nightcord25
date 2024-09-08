875912726f59fb095f6fdf1dc856840b67e92938
alexou <alexandrevalin2@gmail.com>
2024-09-08 12:34:49 -0400

    added blur effect

---
+++ index.js
@@ -0,0 +1,6 @@
+// window.addEventListener("load", tt)
+// function tt(){
+// const clickEl = document.getElementById("hoverable");
+// clickEl.addEventListener("mouseenter", () => {document.body.style.backgroundColor = "red";})
+// clickEl.addEventListener("mouseleave", () => {document.body.style.backgroundColor = "white";})
+// }
